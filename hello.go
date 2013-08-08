c858ce67dea80fbf93211a1b70f48f8fb7c11887
laket <laket@iori.(none)>
2013-08-08 23:03:22 +0900

    大規模変更前

--- hello.go
+++ hello.go
@@ -1,7 +1,97 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Fetcher interface {
+	Fetch(url string) (body string, urls []string, err error)
+}
+
+func Crawl(url string, depth int, fetcher Fetcher, ch chan string) {
+	if depth <= 0 {
+		return 
+	}
+
+	_, urls, err := fetcher.Fetch(url)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//fmt.Printf("found: %s %q\n", url, body)
+
+	if _, ok := dict[url]; !ok {
+		ch <- url
+		dict[url] = true
+	}
+
+	for _, u := range urls {
+		Crawl(u, depth-1, fetcher, ch)
+	}
+
+	return
+}
+
+var dict = make(map[string]bool)
 
 func main() {
-	fmt.Printf("Hello, world\n")
+	ch := make(chan string, 1000)
+	
+	Crawl("http://golang.org/", 4, fetcher, ch)
+	close(ch)
+
+	for url := range(ch) {
+		dict[url] = true
+		fmt.Println(url)
+	}
+}
+
+type fakeFetcher map[string]*fakeResult
+
+type fakeResult struct {
+	body string
+	urls []string
+}
+
+func (f *fakeFetcher) Fetch(url string) (string, []string, error) {
+    if res, ok := (*f)[url]; ok {
+        return res.body, res.urls, nil
+    }
+    return "", nil, fmt.Errorf("not found: %s", url)
+}
+
+// fetcher is a populated fakeFetcher.
+var fetcher = &fakeFetcher{
+    "http://golang.org/": &fakeResult{
+        "The Go Programming Language",
+        []string{
+            "http://golang.org/pkg/",
+            "http://golang.org/cmd/",
+        },
+    },
+    "http://golang.org/pkg/": &fakeResult{
+        "Packages",
+        []string{
+            "http://golang.org/",
+            "http://golang.org/cmd/",
+            "http://golang.org/pkg/fmt/",
+            "http://golang.org/pkg/os/",
+        },
+    },
+    "http://golang.org/pkg/fmt/": &fakeResult{
+        "Package fmt",
+        []string{
+            "http://golang.org/",
+            "http://golang.org/pkg/",
+        },
+    },
+    "http://golang.org/pkg/os/": &fakeResult{
+        "Package os",
+        []string{
+            "http://golang.org/",
+            "http://golang.org/pkg/",
+        },
+    },
 }
